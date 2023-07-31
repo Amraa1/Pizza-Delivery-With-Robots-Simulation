976cad9b60830dd3faeaf0b04820e08f89917382
Amraa1 <amarsanaaganbaatar0409@gmail.com>
2023-07-31 23:15:51 +0100

    First commit

---
+++ int main().cpp
@@ -0,0 +1,25 @@
+int main()
+
+{
+
+    int i = 0;
+
+    switch (i)
+
+    {
+
+        case '0': printf("Formula");
+
+                break;
+
+        case '1': printf("One");
+
+                break;
+
+        default: printf("World Champions");
+
+    }
+
+    return 0;
+
+}
